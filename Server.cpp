b0d10ddba38fc3c374eeb54eb820ed1b46d7c97a
Nakidai <plaza521@inbox.ru>
2024-05-28 19:05:49 +0300

    Merge server with shooter
    Separation of shooter and server into 2 repositories is not a good idea,
    because you need to update them together if you update network code.
    Considering that client can run server too it became too meaningless and
    it would be convenient to merge them.

---
+++ Server.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Иван Ильин on 22.09.2021.
+//
+
+#include <fstream>
+#include <utility>
+#include <iostream>
+#include "network/ShooterServer.h"
+#include <utils/Time.h>
+#include <utils/Log.h>
+#include <Consts.h>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using std::chrono::system_clock;
+
+void InitServer(ShooterServer& server)
+{
+    sf::Uint16 serverPort;
+    std::ifstream connectFile("server.txt", std::ifstream::in);
+
+    if (!connectFile.is_open() || !(connectFile >> serverPort))
+    {
+        connectFile.close();
+        // Create file and write default settings
+        serverPort = 54000;
+        std::ofstream temp("server.txt", std::ofstream::out);
+        temp << serverPort;
+        temp.close();
+    }
+    connectFile.close();
+
+    server.start(serverPort);
+    if(server.isWorking())
+        server.generateBonuses();
+}
+
+int main() {
+    Time::init();
+    ShooterServer server{};
+
+    Log::log("Initializing the server...");
+    InitServer(server);
+
+    double lastTryReconnecting = 0;
+
+    while (true) {
+        if(!server.isWorking() && (Time::time() - lastTryReconnecting > Consts::NETWORK_TIMEOUT)) {
+            lastTryReconnecting = Time::time();
+            server.stop();
+            Log::log("Restarting the server...");
+            InitServer(server);
+        }
+        Time::update();
+        server.update();
+
+        sleep_for(10ms);
+    }
+
+    return 0;
+}
